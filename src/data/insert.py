326fe53ee4974b2307bc3006bb1941cebf92b20c
xiafeng <feng.xia@omnidata.io>
2019-04-21 22:54:53 +0800

    modify add member info

--- src/data/insert.py
+++ src/data/insert.py
@@ -16,12 +16,12 @@ class InsertData(object):
     def add_member_info(self):
         sql = 'insert into user (userId, name, passwd, formerName, idNo, typeId, sex, age, classId, birthday, ' \
             'national, nativePlace, politicalLandscape, admissionDate, mail, schoolYear, memo) ' \
-                'values ("{0}", "{1}", "{2}", "{3}", "{4}", "{5}", "{6}", ' \
-              '"{7}", "{8}", "{9}", "{10}", "{11}", "{12}", "{13}", "{14}", "{15}", "{16}")'.format( \
-            self.__params['userId'], self.__params['name'], self.__params['passwd'], self.__params['formerName'], \
-            self.__params['idNo'], self.__params['typeId'], self.__params['sex'], self.__params['age'], self.__params['classId'], \
-            self.__params['birthday'], self.__params['national'], self.__params['nativePlace'], self.__params['politicalLandscape'], \
-            self.__params['admissionDate'], self.__params['mail'], self.__params['schoolYear'], self.__params['memo'] )
+                'select max(userId)+1, "{0}", max(userId)+1, "{1}", "{2}", "{3}", "{4}", "{5}", ' \
+              '"{6}", "{7}", "{8}", "{9}", "{10}", "{11}", "{12}", "{13}", "{14}" from user where typeId = "{15}" '.format( \
+            self.__params['name'], self.__params['formerName'], self.__params['idNo'], self.__params['typeId'], \
+            self.__params['sex'], self.__params['age'], self.__params['classId'], self.__params['birthday'], \
+            self.__params['national'], self.__params['nativePlace'], self.__params['politicalLandscape'], self.__params['admissionDate'], \
+            self.__params['mail'], self.__params['schoolYear'], self.__params['memo'], self.__params['typeId'] )
         result = self.__cursor.execute(sql)
         if result.rowcount > 0:
             return True
