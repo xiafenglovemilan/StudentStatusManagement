b6e86335a70f942b7f227acf352d30afe8ad9a3f
xiafeng <feng.xia@omnidata.io>
2019-04-27 18:07:15 +0800

    modify interface modifyuserinfo

--- src/http/modInfo.py
+++ src/http/modInfo.py
@@ -9,7 +9,6 @@ async def mod_user_info(request):
     session = await get_session(request)
     session_name = session['userId'] if 'userId' in session else None
     data = await request.json()
-    data['userId'] = session_name
     if session_name:
         sel_obj = update.UpdateData(params=data)
         is_success = sel_obj.update_user_info()
