8448bb31bb6c7b36ea7993bc4abb4132f586c225
xiafeng <feng.xia@omnidata.io>
2019-05-18 18:47:02 +0800

    modify interface getuserlist bug

--- src/data/select.py
+++ src/data/select.py
@@ -163,7 +163,7 @@ class SelectData(object):
 
     def select_user_list(self):
         sql = 'select userId, name from user where typeId = "{0}"'
-        if 'classId' in self.__params:
+        if self.__params['typeId'] == 4 and 'classId' in self.__params:
             sql += 'and classId = "{0}"'.format(self.__params['classId'])
         sql_format = sql.format(self.__params['typeId'])
         result = self.__cursor.execute(sql_format)
