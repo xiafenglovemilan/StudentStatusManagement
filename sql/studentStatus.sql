894c9ce864fcb54544a6e897bec17b8612f11fc5
xiafeng <feng.xia@omnidata.io>
2019-05-20 17:29:19 +0800

    modify sql

--- sql/studentStatus.sql
+++ sql/studentStatus.sql
@@ -1,26 +1,50 @@
 --
--- 由SQLiteStudio v3.2.1 产生的文件 周一 3月 25 14:46:04 2019
+-- 由SQLiteStudio v3.2.1 产生的文件 周一 5月 20 17:28:04 2019
 --
 -- 文本编码：UTF-8
 --
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
+-- 表：class
+DROP TABLE IF EXISTS class;
+CREATE TABLE class (id INTEGER PRIMARY KEY, describe VARCHAR NOT NULL, professId INTEGER NOT NULL);
+
+-- 表：classTime
+DROP TABLE IF EXISTS classTime;
+CREATE TABLE classTime (id INTEGER PRIMARY KEY AUTOINCREMENT, describe VARCHAR, week INTEGER);
+
+-- 表：course
+DROP TABLE IF EXISTS course;
+CREATE TABLE course (id INTEGER PRIMARY KEY AUTOINCREMENT, describe VARCHAR);
+
+-- 表：courseTable
+DROP TABLE IF EXISTS courseTable;
+CREATE TABLE courseTable (id INTEGER PRIMARY KEY AUTOINCREMENT, classTimeId INTEGER, userId INTEGER, courseId INTEGER, classId INTEGER, semesterId INTEGER);
+
+-- 表：departments
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments (id INTEGER PRIMARY KEY AUTOINCREMENT, describe VARCHAR UNIQUE NOT NULL);
+
 -- 表：grade
 DROP TABLE IF EXISTS grade;
-CREATE TABLE grade (id INTEGER PRIMARY KEY AUTOINCREMENT, userId INTEGER NOT NULL, chinese DOUBLE, english DOUBLE, math DOUBLE, physics DOUBLE, chemistry DOUBLE, biology DOUBLE);
+CREATE TABLE grade (id INTEGER PRIMARY KEY AUTOINCREMENT, userId INTEGER NOT NULL, courseId INTEGER, grade DOUBLE, semesterId INTEGER);
 
--- 表：permission
-DROP TABLE IF EXISTS permission;
-CREATE TABLE permission (id INTEGER PRIMARY KEY AUTOINCREMENT, describe VARCHAR (50) NOT NULL);
+-- 表：professional
+DROP TABLE IF EXISTS professional;
+CREATE TABLE professional (id INTEGER PRIMARY KEY AUTOINCREMENT, describe VARCHAR, departId INTEGER);
+
+-- 表：semester
+DROP TABLE IF EXISTS semester;
+CREATE TABLE semester (id INTEGER PRIMARY KEY AUTOINCREMENT, describe VARCHAR);
 
 -- 表：user
 DROP TABLE IF EXISTS user;
-CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR (50), typeId INTEGER NOT NULL, college VARCHAR (50), sex BOOLEAN, age INT, major VARCHAR (50), class VARCHAR (50), birthday DATE);
+CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, userId INTEGER, name VARCHAR (50) NOT NULL, passwd VARCHAR (10) NOT NULL, formerName VARCHAR, idNo VARCHAR, typeId INTEGER NOT NULL, sex CHAR, age INT, classId INTEGER, birthday DATE, national VARCHAR, nativePlace VARCHAR, politicalLandscape VARCHAR, admissionDate DATE, mail VARCHAR, schoolYear VARCHAR, memo VARCHAR);
 
 -- 表：userType
 DROP TABLE IF EXISTS userType;
-CREATE TABLE userType (id INTEGER PRIMARY KEY AUTOINCREMENT, describe VARCHAR (50) NOT NULL, permission_id INT NOT NULL);
+CREATE TABLE userType (id INTEGER PRIMARY KEY AUTOINCREMENT, describe VARCHAR (50) NOT NULL);
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
